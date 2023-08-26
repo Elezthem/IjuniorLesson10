53a06349127d979a6ca9aa6a66b8664f44c5184a
HemyDev <97851451+Elezthem@users.noreply.github.com>
2023-08-26 09:57:01 +0300

    Add files via upload

---
+++ Ijunior/Program.cs
@@ -0,0 +1,23 @@
+﻿bool thimble1 = false;
+bool thimble2 = false;
+bool thimble3 = true;
+
+Console.WriteLine("Игра в наперстки");
+Console.WriteLine();
+Console.WriteLine("под одним из трех наперстков находится шарик. Попробуйте угадать в каком!");
+Console.WriteLine();
+Console.Write("Введите ваше число от 1 до 3: ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+bool result = false;
+
+switch (input) { case 1: result = thimble1; break; case 2: result = thimble2; break; case 3: result = thimble3; break; }
+
+if (result == true)
+{
+    Console.WriteLine("Да! Вы нашли шарик!");
+}
+else
+{
+    Console.WriteLine("Нет! Вы не угадали!");
+}
